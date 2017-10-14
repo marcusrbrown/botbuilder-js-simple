2fd2c627a8e6273b56e12f6509dbd5e0b282de8f
Marcus R. Brown <me@igetgam.es>
2017-10-14 11:11:54 -0700

    Add restify and support the BF emulator.

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const builder = require('botbuilder');
+const restify = require('restify');
 
-const connector = new builder.ConsoleConnector().listen();
+const server = restify.createServer();
+
+server.listen(process.env.port || process.env.PORT || 3978,
+  () => console.log(`${server.name} listening to ${server.url}`));
+
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
 const bot = new builder.UniversalBot(connector, session => session.send(`You said ${session.message.text}`));
